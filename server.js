2a4148c106a17191f6b19155e0fd575bbdb92b74
nelly <nellylusine@gmail.com>
2020-09-02 21:44:57 +0400

    Add compression for files

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const compression = require('compression');
 const app = express();
+
+
+app.use(compression());
 
 app.use(express.static('./dist/self-edu-web'));
 app.get('/*', (req, res) => res.sendFile('src/index.html', { root: 'dist/self-edu-web'}));
